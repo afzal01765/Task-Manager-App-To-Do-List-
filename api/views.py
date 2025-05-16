a85b6cf574bdedebc0638a8f95abe08112607bcf
afzal khan <afzalkhan01765@gmail.com>
2025-05-16 22:59:08 +0600

    file ad

--- api/views.py
+++ api/views.py
@@ -1,21 +1,42 @@
-from api.models import Category , TodoTask
-from api.serializers import SnippetSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Task
 
+# List all tasks
+class TaskListView(ListView):
+    model = Task
+    template_name = 'tasks/task_list.html'
+    context_object_name = 'tasks'
 
-class TodoTaskList(APIView):
-    
-    def get(self, request, format=None):
-        TodoTask = TodoTask.objects.all()
-        serializer = SnippetSerializer(TodoTask, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
 
-    def post(self, request, format=None):
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# Show task details
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'tasks/task_detail.html'
+    context_object_name = 'task'
+
+# Create a new task
+class TaskCreateView(CreateView):
+    model = Task
+    fields = ['title', 'description', 'completed']
+    template_name = 'tasks/task_form.html'
+    success_url = reverse_lazy('task-list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+# Update an existing task
+class TaskUpdateView(UpdateView):
+    model = Task
+    fields = ['title', 'description', 'completed']
+    template_name = 'tasks/task_form.html'
+    success_url = reverse_lazy('task-list')
+
+# Delete a task
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'tasks/task_confirm_delete.html'
+    success_url = reverse_lazy('task-list')
